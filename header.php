2ebe43fd10a5baadadd4d9ec24947f5f9e07f1b7
Mookie Kong <mookie@ymail.com>
2009-01-24 14:00:31 -0800

    added comments link

--- header.php
+++ header.php
@@ -18,9 +18,9 @@ $siteurl = getSiteUrl();
 <?php
 	if(checkCookie()) {
 		$username = getUserName();
-		echo "<a href=\"$siturl/usermod.php\" class=\"menu\">" . $username . "</a> | <a href=\"$siteurl/post.php\" class=\"menu\">new post</a> | posts: " . $numOfEntries . " | <a href=\"$siteurl/settings.php\" class=\"menu\">site admin</a> | <a href=\"$siteurl/rss.php\" class=\"menu\">rss</a> | <a href=\"$siteurl/logout.php\" class=\"menu\">logout</a>";
+		echo "<a href=\"$siturl/usermod.php\" class=\"menu\">" . $username . "</a> | <a href=\"$siteurl/post.php\" class=\"menu\">new post</a> | posts: " . $numOfEntries . " | <a href=\"$siteurl/settings.php\" class=\"menu\">site admin</a> | <a href=\"$siteurl/comments.php\" class=\"menu\">comments</a> | <a href=\"$siteurl/rss.php\" class=\"menu\">rss</a> | <a href=\"$siteurl/logout.php\" class=\"menu\">logout</a>";
 	} else {
-		echo "updates: " . $numOfEntries . " | <a href=\"$siteurl/login.php\" class=\"menu\">login</a> | <a href=\"$siteurl/rss.php\" class=\"menu\">rss</a>";
+		echo "updates: " . $numOfEntries . " | <a href=\"$siteurl/login.php\" class=\"menu\">login</a> | <a href=\"$siteurl/comments.php\" class=\"menu\">comments</a> | <a href=\"$siteurl/rss.php\" class=\"menu\">rss</a>";
 	}
 ?>
 </p>
