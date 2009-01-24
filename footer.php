b4f97408094dff6a875efcc224b6fe71f1160ee5
Mookie Kong <mookie@ymail.com>
2009-01-24 11:20:05 -0800

    changed links to point to dertyn.com

--- footer.php
+++ footer.php
@@ -1,6 +1,6 @@
 <hr />
 <p class="footer">
-powered by <a href="http://github.com/ultramookie/dertyn/">dertyn</a>.<br />
+powered by <a href="http://dertyn.com/">dertyn</a>.<br />
 </p>
 </div>
 </body>
