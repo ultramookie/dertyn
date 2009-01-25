2eb082ae1ffeb147883f6ef37301b68d34974e37
Mookie Kong <mookie@ymail.com>
2009-01-24 22:58:47 -0800

    removed copyright from footer

--- footer.php
+++ footer.php
@@ -1,6 +1,6 @@
 </div>
 <div id="footer">
-powered by <a href="http://dertyn.com/">dertyn</a>. copyright (c) 2009 steve "<a href="http://ultramookie.com">mookie</a>" kong<br />
+powered by <a href="http://dertyn.com/">dertyn</a>.
 </div>
 </div>
 </body>
