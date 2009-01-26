455d6dd96c8ae5cf30155acfe6d598bb82d6d97c
Mookie Kong <mookie@ymail.com>
2009-01-25 22:12:22 -0800

    moving sidebar items around

--- sidebar.php
+++ sidebar.php
@@ -6,21 +6,20 @@
 	echo "<li>posts: $numOfEntries</li>\n";
 	echo "<li><a href=\"$siteurl/comments.php\">comments</a>: $totalComments</li>\n";
 	echo "<li><a href=\"$siteurl/rss.php\">rss</a></li>\n";
-	if(!checkCookie()) {
-		echo "<li><a href=\"$siteurl/login.php\">login</a></li>\n";
-	}
 	echo "</ul>\n";
 
+	echo "admin menu\n";
+	echo "<ul>\n";
 	if(checkCookie()) {
-		echo "admin menu\n";
-		echo "<ul>\n";
-		echo "<li><a href=\"$siteurl/post.php\">post</a></li>\n";
+		echo "<li><a href=\"$siteurl/post.php\">new post</a></li>\n";
 		echo "<li><a href=\"$siteurl/drafts.php\">drafts</a></li>\n";
 		echo "<li><a href=\"$siturl/usermod.php\">password</a></li>\n";
 		echo "<li><a href=\"$siteurl/settings.php\">settings</a></li>\n";
 		echo "<li><a href=\"$siteurl/logout.php\">logout</a></li>\n";
-		echo "</ul>\n";
+	} else {
+		echo "<li><a href=\"$siteurl/login.php\">login</a></li>\n";
 	}
+	echo "</ul>\n";
 
 ?>
 </div>
