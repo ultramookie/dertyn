7680bab15e954c308c0284b4c17bedd50f5a9083
Mookie Kong <mookie@ymail.com>
2009-02-01 23:23:53 -0800

    finishing off getting this fully validated (added stuff from "recommendations" section).

--- rss.php
+++ rss.php
@@ -12,12 +12,14 @@ echo "<?xml version=\"1.0\" encoding=\"UTF-8\"?>";
 
 <rss version="2.0"
 	xmlns:content="http://purl.org/rss/1.0/modules/content/"
+	xmlns:atom="http://www.w3.org/2005/Atom"
 >
   <channel>
 	<title><?php echo $sitename; ?></title>
 	<link><?php echo $siteurl; ?></link>
 	<description><?php echo $sitename; ?></description>
 	<generator>dertyn <?php echo $version; ?></generator>
+	<atom:link href="<?php echo $siteurl; ?>/rss.php" rel="self" type="application/rss+xml" />
 	<ttl>5</ttl>
 <?php
 	printRSS($numRss);
