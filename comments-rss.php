09928d39b2df03a27162f88d9aec7b9ffe1d18f3
Mookie Kong <mookie@ymail.com>
2009-02-01 23:26:00 -0800

    finished off validation stuff (did the "recommendations").

--- comments-rss.php
+++ comments-rss.php
@@ -12,12 +12,14 @@ echo "<?xml version=\"1.0\" encoding=\"UTF-8\"?>";
 
 <rss version="2.0"
 	xmlns:content="http://purl.org/rss/1.0/modules/content/"
+	xmlns:atom="http://www.w3.org/2005/Atom"
 >
   <channel>
 	<title><?php echo $sitename . " comments"; ?></title>
 	<link><?php echo $siteurl . "/comments.php"; ?></link>
 	<description><?php echo $sitename . " comments from readers"; ?></description>
 	<generator>dertyn <?php echo $version; ?></generator>
+	<atom:link href="<?php echo $siteurl; ?>/comments-rss.php" rel="self" type="application/rss+xml" />
 	<ttl>5</ttl>
 <?php
 	printCommentsRSS($numRss);
