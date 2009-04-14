d1064baac0c5e0dca717cc96246affa09f9c59c7
Mookie Kong <mookie@ymail.com>
2009-04-13 18:08:49 -0700

    changed utf8 call for compatiblity reasons (aka php < 5.2)

--- db.php
+++ db.php
@@ -5,8 +5,8 @@ include_once("config.php");
 $link = mysql_connect("$dbhost","$dbuser","$dbpass")
     or die('Could not connect: ' . mysql_error());
 
-mysql_set_charset('utf8',$link);
+mysql_query("SET character_set_results = 'utf8', character_set_client = 'utf8', character_set_connection = 'utf8', character_set_database = 'utf8', character_set_server = 'utf8'", $link);
 
-mysql_select_db("$db") or die('Could not select database');
+ mysql_select_db("$db") or die('Could not select database');
 
 ?>
